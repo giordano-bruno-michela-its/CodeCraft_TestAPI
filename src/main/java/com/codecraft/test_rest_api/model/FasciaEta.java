61c5a591541d4c8ae57e08b99b365d185f9ddfe5
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2024-12-10 13:05:42 +0100

    Minor fix - removed old const/getters/setters

--- src/main/java/com/codecraft/test_rest_api/model/FasciaEta.java
+++ src/main/java/com/codecraft/test_rest_api/model/FasciaEta.java
@@ -19,47 +19,4 @@ public class FasciaEta {
     private String descrizione;
     private int etaMin;
     private int etaMax;
-
-//    public FasciaEta() {
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getDenominazione() {
-//        return denominazione;
-//    }
-//
-//    public void setDenominazione(String denominazione) {
-//        this.denominazione = denominazione;
-//    }
-//
-//    public String getDescrizione() {
-//        return descrizione;
-//    }
-//
-//    public void setDescrizione(String descrizione) {
-//        this.descrizione = descrizione;
-//    }
-//
-//    public int getEtaMin() {
-//        return etaMin;
-//    }
-//
-//    public void setEtaMin(int etaMinima) {
-//        this.etaMin = etaMinima;
-//    }
-//
-//    public int getEtaMax() {
-//        return etaMax;
-//    }
-//
-//    public void setEtaMax(int etaMassima) {
-//        this.etaMax = etaMassima;
-//    }
 }
--- src/main/java/com/codecraft/test_rest_api/model/TipoAttivita.java
+++ src/main/java/com/codecraft/test_rest_api/model/TipoAttivita.java
@@ -17,23 +17,4 @@ public class TipoAttivita {
     private Long id;
     
     private String denominazione;
-
-//    public TipoAttivita() {
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getDenominazione() {
-//        return denominazione;
-//    }
-//
-//    public void setDenominazione(String denominazione) {
-//        this.denominazione = denominazione;
-//    }
 }
